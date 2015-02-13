518aca4a8f3b20f996a75bb8ce1b357692e613b5
Charles Hart <charleshart.lds@gmail.com>
2015-02-12 17:10:30 -0700

    First commit, brand new project. Modify .gitignore

---
+++ Launchpad-chat/Launchpad-chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Launchpad-chat
+//
+//  Created by Charles Hart on 2/12/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
