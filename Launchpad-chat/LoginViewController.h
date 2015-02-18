171844762786d0284b971d3d65a7e3f746a3e520
STUDENT #3 <student3@STEMs-MacBook-Air-2.local>
2015-02-17 18:16:51 -0700

    Add LoginViewController

---
+++ Launchpad-chat/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Launchpad-chat
+//
+//  Created by STUDENT #3 on 2/12/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UICollectionViewController
+
+@end
