690de9448cb750151966bdbd6ffdc428c3bbefae
Collin Walker <cw404@hotmail.com>
2015-02-18 12:28:04 -0700

    Add User Index Class

---
+++ Launchpad-chat/Launchpad-chat/UserIndexTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserIndexTableViewController.h
+//  Launchpad-chat
+//
+//  Created by Collin on 2/12/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserIndexTableViewController : UITableViewController
+
+@end
