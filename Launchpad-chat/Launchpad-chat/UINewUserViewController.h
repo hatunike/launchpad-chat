fe3eb286fc94ddb00d282189ec4631ea48dec288
STUDENT #6 <student6@STEMs-MacBook-Air-4.local>
2015-02-19 17:34:59 -0700

    Added View Controller Classes

---
+++ Launchpad-chat/Launchpad-chat/UINewUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  UINewUserViewController.h
+//  Launchpad-chat
+//
+//  Created by STUDENT #6 on 2/19/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINewUserViewController : UIViewController
+
+@end
