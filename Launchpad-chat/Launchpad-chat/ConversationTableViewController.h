29e5e81dd9bd78c3ff64dde190ea29f106f43da1
Tyler Brinkerhoff <thesaladcartographer@gmail.com>
2015-02-24 16:47:50 -0700

    Adding conversationviewcontroller

---
+++ Launchpad-chat/Launchpad-chat/ConversationTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConversationTableViewController.h
+//  Launchpad-chat
+//
+//  Created by STUDENT #4 on 2/12/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConversationTableViewController : UITableViewController
+
+@end
