c8abb17bfe6e249a87878a8313566aea63971499
STUDENT #6 <student6@STEMs-MacBook-Air-4.local>
2015-02-24 20:17:29 -0700

    More UI Design Changes

---
+++ Launchpad-chat/Launchpad-chat/LoginTextField.h
@@ -0,0 +1,13 @@
+//
+//  LoginTextField.h
+//  Launchpad-chat
+//
+//  Created by STUDENT #6 on 2/24/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginTextField : UITextField
+
+@end
---
+++ Launchpad-chat/Launchpad-chat/LoginTextField.m
@@ -0,0 +1,21 @@
+//
+//  LoginTextField.m
+//  Launchpad-chat
+//
+//  Created by STUDENT #6 on 2/24/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import "LoginTextField.h"
+
+@implementation LoginTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
