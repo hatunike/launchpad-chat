2179025650562c0161ed506a59a36f0e9bd9ca95
Tyler Brinkerhoff <thesaladcartographer@gmail.com>
2015-02-24 17:15:13 -0700

    readding conversationtableviewcontroller

--- Launchpad-chat/Launchpad-chat/ConversationTableViewController.m
+++ Launchpad-chat/Launchpad-chat/ConversationTableViewController.m
@@ -2,7 +2,7 @@
 //  ConversationTableViewController.m
 //  Launchpad-chat
 //
-//  Created by STUDENT #4 on 2/12/15.
+//  Created by STUDENT #4 on 2/24/15.
 //  Copyright (c) 2015 lernu. All rights reserved.
 //
 
