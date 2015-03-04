b460cbb72b217818a6eb883bb04f73105598d7a2
Marchelle Lundquist <marchellelundquist@gmail.com>
2015-03-03 18:01:41 -0700

    Make changes to storyboard, add custom cell class and update outlets.

---
+++ Launchpad-chat/Launchpad-chat/UIUserTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  UIUserTableViewCell.h
+//  Launchpad-chat
+//
+//  Created by Marchelle Lundquist on 3/3/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIUserTableViewCell : UITableViewCell
+
+@end
---
+++ Launchpad-chat/Launchpad-chat/UIUserTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  UIUserTableViewCell.m
+//  Launchpad-chat
+//
+//  Created by Marchelle Lundquist on 3/3/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import "UIUserTableViewCell.h"
+
+@implementation UIUserTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
