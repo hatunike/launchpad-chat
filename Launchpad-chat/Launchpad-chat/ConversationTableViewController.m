30cc9c511e5fef310c83e70888f2f765b307c53b
Charles Hart <charleshart.lds@gmail.com>
2015-03-10 18:05:50 -0600

    add messaging work to controllers

--- Launchpad-chat/Launchpad-chat/ConversationTableViewController.m
+++ Launchpad-chat/Launchpad-chat/ConversationTableViewController.m
@@ -8,9 +8,14 @@
 
 #import "ConversationTableViewController.h"
 #import "UserTableViewController.h"
+#import "DialogTableViewController.h"
+#import "AppDelegate.h"
+#import "Message+Additions.h"
+#import "User+Additions.h"
 
 @interface ConversationTableViewController ()
 
+@property (strong, nonatomic) IBOutlet UITextField *messageTextField;
 
 @end
 
@@ -19,91 +24,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.context = [(AppDelegate*)[[UIApplication sharedApplication] delegate] managedObjectContext];
     self.userNameHeader.text = self.userName;
+    self.messageTextField.delegate = self;
+
+}
+- (IBAction)sendMessageButtonPressed:(id)sender
+{
+    User* currentUser = [[self.context executeFetchRequest:[User requestUserWithName:self.userName] error:nil] lastObject];
     
+    [Message createMessageWithText:self.messageTextField.text onDate:[NSDate date] fromUser:currentUser inConversation:nil withState:YES inContext:self.context];
+    self.messageTextField.text = @"";
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 - (IBAction)dismissConversationViewController:(id)sender
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
 
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self sendMessageButtonPressed:nil];
+    return NO;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
-}
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"Dialog TableViewController"])
+    {
+        self.context = [(AppDelegate*)[[UIApplication sharedApplication] delegate] managedObjectContext];
+        DialogTableViewController* vc = [segue destinationViewController];
+        NSFetchRequest* fr = [NSFetchRequest fetchRequestWithEntityName:@"Message"];
+        fr.predicate = [NSPredicate predicateWithFormat:@"fromWho.name == %@", self.userName];
+        fr.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"date" ascending:YES]];
+        
+        NSFetchedResultsController *frc = [[NSFetchedResultsController alloc] initWithFetchRequest:fr managedObjectContext:self.context sectionNameKeyPath:nil cacheName:nil];
+        
+        [vc setFetchedResultsController:frc];
+    }
+    
 }
-*/
+
 
 @end
---
+++ Launchpad-chat/Launchpad-chat/DialogTableViewController.m
@@ -0,0 +1,44 @@
+//
+//  DialogTableViewController.m
+//  Launchpad-chat
+//
+//  Created by Charles Hart on 3/10/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import "DialogTableViewController.h"
+#import "Message+Additions.h"
+
+@interface DialogTableViewController ()
+
+@end
+
+@implementation DialogTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+    
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    Message* message = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.textLabel.text = message.text;
+    return cell;
+}
+
+
+
+
+
+@end
