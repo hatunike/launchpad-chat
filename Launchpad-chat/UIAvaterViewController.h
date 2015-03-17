7025573550616fae9a6863487250366c2460a637
Derek Hoyt <derekhoyt1@gmail.com>
2015-03-17 17:50:11 -0600

    add Avater controller

---
+++ Launchpad-chat/UIAvaterViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIAvaterViewController.h
+//  Launchpad-chat
+//
+//  Created by STUDENT #5 on 3/17/15.
+//  Copyright (c) 2015 lernu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAvaterViewController : UIViewController
+
+@end
